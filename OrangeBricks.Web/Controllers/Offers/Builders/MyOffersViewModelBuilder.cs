4065d8147dc18c145a97d779a3aeeccd20de4c15
Brendan Ward <brendan.ward@gmail.com>
2017-04-30 15:38:33 -0400

    Adding My Offers View to Buyer Role

---
+++ OrangeBricks.Web/Controllers/Offers/Builders/MyOffersViewModelBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Data.Entity;
+using System.Collections.Generic;
+using OrangeBricks.Web.Controllers.Offers.ViewModels;
+using OrangeBricks.Web.Models;
+
+namespace OrangeBricks.Web.Controllers.Offers.Builders
+{
+    public class MyOffersViewModelBuilder
+    {
+        private readonly IOrangeBricksContext _context;
+
+        public MyOffersViewModelBuilder(IOrangeBricksContext context)
+        {
+            _context = context;
+        }
+
+        public MyOffersViewModel Build(string buyerId)
+        {
+            var offers = _context.Offers
+                .Where(o => o.BuyerUserId == buyerId)
+                .Select(o => new OfferViewModel
+                {
+                    Id = o.Id,
+                    Amount = o.Amount,
+                    CreatedAt = o.CreatedAt,
+                    Status = o.Status.ToString()
+                })
+                .ToList();
+
+            var myOfferViewModel = new MyOffersViewModel { Offers = offers };
+
+            return myOfferViewModel;
+        }
+    }
+}
---
+++ OrangeBricks.Web/Controllers/Offers/ViewModels/MyOffersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace OrangeBricks.Web.Controllers.Offers.ViewModels
+{
+    public class MyOffersViewModel
+    {
+        public List<OfferViewModel> Offers { get; set; }
+    }
+}
